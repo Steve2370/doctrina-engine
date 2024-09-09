edb96c09e9e0d019d932f0f5c70bc536e41e659d
Steve2370 <briceyouatchui@gmail.com>
2024-09-09 10:01:54 -0400

    refactor: start method is final

--- src/BouncingBallGame.java
+++ src/BouncingBallGame.java
@@ -1,9 +1,11 @@
 import java.awt.*;
 
-public class BouncingBallGame extends Game {
+public final class BouncingBallGame extends Game {
 
     private int score;
     private Ball ball;
+
+
 
     @Override
     protected void intialize() {
