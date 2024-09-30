a4c785f421a529d82a5c23dc78bb8ff5df2e3fae
Steve2370 <briceyouatchui@gmail.com>
2024-09-30 09:22:23 -0400

    feat: add footprints to player

--- src/FootPrint/FootprintGame.java
+++ src/FootPrint/FootprintGame.java
@@ -4,15 +4,18 @@ import Doctrina.Canvas;
 import Doctrina.Game;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class FootprintGame extends Game {
     private GamePad gamepad;
     private Player player;
+    private ArrayList<Footprint> footprints;
 
     @Override
     protected void intialize() {
         gamepad = new GamePad();
         player = new Player(gamepad);
+        footprints = new ArrayList<>();
     }
 
     @Override
@@ -21,11 +24,17 @@ public class FootprintGame extends Game {
             stop();
         }
         player.update();
+        if (gamepad.isMoving()) {
+            footprints.add(player.layFootprint());
+        }
     }
 
     @Override
     protected void draw(Canvas canvas) {
         canvas.drawRectangle(0, 0, 800,600, Color.BLUE);
+        for (Footprint footprint : footprints) {
+            footprint.draw(canvas);
+        }
         player.draw(canvas);
     }
 }
