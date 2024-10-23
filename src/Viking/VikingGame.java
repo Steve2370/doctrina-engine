e7bad1b88ad397275801c9a3ab8d075457a9ebaa
Steve2370 <briceyouatchui@gmail.com>
2024-10-23 15:17:04 -0400

    feat: implement sprite sheet

--- src/Viking/VikingGame.java
+++ src/Viking/VikingGame.java
@@ -3,10 +3,17 @@ package Viking;
 import Doctrina.Canvas;
 import Doctrina.Game;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class VikingGame extends Game {
     private Player player;
     private GamePad gamePad;
     private World world;
+    private Tree tree;
+
+    private int soundCoolDown;
 
     @Override
     protected void intialize() {
@@ -14,6 +21,19 @@ public class VikingGame extends Game {
         player = new Player(gamePad);
         world = new World();
         world.load();
+        tree = new Tree();
+
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                    getClass().getClassLoader().getResourceAsStream("audios/music.wav")
+            );
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -23,11 +43,38 @@ public class VikingGame extends Game {
         }
 
         player.update();
+
+        soundCoolDown --;
+        if (soundCoolDown < 0) {
+            soundCoolDown = 0;
+        }
+
+        if (gamePad.isFirePressed() && soundCoolDown == 0) {
+            soundCoolDown = 100;
+
+            // FIRE
+            try {
+                Clip clip = AudioSystem.getClip();
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                        getClass().getClassLoader().getResourceAsStream("audios/fire.wav")
+                );
+                clip.open(audioInputStream);
+                clip.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     protected void draw(Canvas canvas) {
         world.draw(canvas);
-        player.draw(canvas);
+
+        // Tree Height - Switch Layer
+        // 80 - 28 (max layer) = 52
+        if (player.getY() < tree.getY() + 52) {
+            player.draw(canvas);
+            tree.draw(canvas);
+        }
     }
 }
